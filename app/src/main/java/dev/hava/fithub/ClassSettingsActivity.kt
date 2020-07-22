69be99f13f1394b48c021d06074d5e77509e3f81
Amir Abbas <am_882011@live.com>
2020-07-22 12:38:26 +0430

    add activities

---
+++ app/src/main/java/dev/hava/fithub/ClassSettingsActivity.kt
@@ -0,0 +1,13 @@
+package dev.hava.fithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.hava.fithub.api.Instance
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ClassSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class_settings)
+    }
+}
---
+++ app/src/main/java/dev/hava/fithub/PostActivity.kt
@@ -0,0 +1,13 @@
+package dev.hava.fithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.hava.fithub.api.Instance
+import kotlinx.android.synthetic.main.activity_main.*
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
