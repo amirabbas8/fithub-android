d91bf86eb3d24b79c5a126333150bd7d7a050640
Amir Abbas <am_882011@live.com>
2020-07-24 10:56:15 +0430

    show posts

--- app/src/main/java/dev/hava/fithub/fragments/CourseFragment.kt
+++ app/src/main/java/dev/hava/fithub/fragments/CourseFragment.kt
@@ -7,9 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.RecyclerView
 import dev.hava.fithub.BaseAdapter
-import dev.hava.fithub.models.PostModel
 import dev.hava.fithub.R
+import dev.hava.fithub.api.DefaultCallback
+import dev.hava.fithub.api.Instance
+import dev.hava.fithub.models.PostModel
+import dev.hava.fithub.toast
 import kotlinx.android.synthetic.main.fragment_course.view.*
 import kotlinx.android.synthetic.main.item_post.view.*
 import java.util.*
@@ -22,29 +26,49 @@ class CourseFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_course, container, false)
-        view.posts.adapter =
-            BaseAdapter(
-                listOf(
-                    PostModel(
-                        1,
-                        "hi",
-                        Date().time.toInt()
-                    )
-                ),
-                R.layout.item_post
-            ) { model, itemView ->
-                itemView.text.text = model.text
-                itemView.setOnClickListener {
-                    val action =
-                        CourseFragmentDirections.actionCourseFragmentToPostFragment(model)
-                    findNavController().navigate(action)
-                }
-            }
+        refresh(view.posts)
         view.settings.setOnClickListener {
             val action =
                 CourseFragmentDirections.actionCourseFragmentToCourseSettingsFragment(args.course)
             findNavController().navigate(action)
         }
         return view
+    }
+
+    private fun refresh(rv: RecyclerView) {
+        Instance.getPosts(
+            requireContext(),
+            args.course.courseId,
+            DefaultCallback(requireContext(), {
+                val items = ArrayList<PostModel>()
+                it.body()?.posts?.let { posts ->
+                    for (post in posts) {
+                        val item = post.asJsonObject
+                        val fields = item.get("fields").asJsonObject
+                        items.add(
+                            PostModel(
+                                item.get("pk").asInt,
+                                fields.get("text").asString,
+                                fields.get("order").asInt
+                            )
+                        )
+                    }
+                }
+                rv.adapter =
+                    BaseAdapter(
+                        items,
+                        R.layout.item_post
+                    ) { model, itemView ->
+                        itemView.text.text = model.text
+                        itemView.setOnClickListener {
+                            val action =
+                                CourseFragmentDirections.actionCourseFragmentToPostFragment(model)
+                            findNavController().navigate(action)
+                        }
+                    }
+            }, {
+                toast("خطایی رخ داد.")
+            })
+        )
     }
 }
