a26f16d38e0231c6d6b8eb3df96c044fcadc4910
Amir Abbas <am_882011@live.com>
2020-08-12 10:27:17 +0430

    design

--- app/src/main/java/dev/hava/fithub/fragments/MainFragment.kt
+++ app/src/main/java/dev/hava/fithub/fragments/MainFragment.kt
@@ -1,9 +1,7 @@
 package dev.hava.fithub.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
@@ -13,12 +11,14 @@ import dev.hava.fithub.api.DefaultCallback
 import dev.hava.fithub.api.Instance
 import dev.hava.fithub.models.HistoryModel
 import dev.hava.fithub.toast
+import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 import kotlinx.android.synthetic.main.item_history.view.*
 
 class MainFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
         if (Instance.getUserId(requireContext()) ?: 0 == 0) {
             val action =
                 MainFragmentDirections.actionMainFragmentToLoginFragment()
@@ -51,7 +51,6 @@ class MainFragment : Fragment() {
             findNavController().navigate(action)
         }
         refresh(view.history)
-        view.refresh.setOnClickListener { refresh(view.history) }
         return view
     }
 
@@ -77,14 +76,85 @@ class MainFragment : Fragment() {
                     items,
                     R.layout.item_history
                 ) { model, itemView ->
-                    itemView.historyType.text = model.historyType.toString()
+                    itemView.historyType.text =
+                        resources.getStringArray(R.array.history_types_array)[model.historyType]
                     itemView.historyDetail.text = model.details
                     itemView.historyValue.text = model.value
+                    itemView.historyTypeIV.setImageResource(
+                        when (model.historyType) {
+                            0 -> {
+                                R.drawable.ic_baseline_category_24
+                            }
+                            1 -> {
+                                R.drawable.ic_baseline_healing_24
+                            }
+                            2 -> {
+                                R.drawable.ic_baseline_height_24
+                            }
+                            3 -> {
+                                R.drawable.ic_baseline_face_24
+                            }
+                            4 -> {
+                                R.drawable.ic_baseline_fastfood_24
+                            }
+                            5 -> {
+                                R.drawable.ic_baseline_airline_seat_individual_suite_24
+                            }
+                            6 -> {
+                                R.drawable.ic_baseline_emoji_food_beverage_24
+                            }
+                            7 -> {
+                                R.drawable.ic_baseline_emoji_food_beverage_24
+                            }
+                            8 -> {
+                                R.drawable.ic_baseline_emoji_food_beverage_24
+                            }
+                            9 -> {
+                                R.drawable.ic_baseline_directions_walk_24
+                            }
+                            10 -> {
+                                R.drawable.ic_baseline_directions_bike_24
+                            }
+                            11 -> {
+                                R.drawable.ic_baseline_waves_24
+                            }
+                            12 -> {
+                                R.drawable.ic_baseline_image_24
+                            }
+                            13 -> {
+                                R.drawable.ic_baseline_directions_walk_24
+                            }
+                            14 -> {
+                                R.drawable.ic_baseline_directions_walk_24
+                            }
+                            else -> {
+                                R.drawable.ic_baseline_category_24
+                            }
+                        }
+                    )
                     itemView.setOnClickListener {
                     }
                 }
         }, {
             toast("خطایی رخ داد.")
         }))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.refresh -> {
+                refresh(history)
+                true
+            }
+            R.id.logout -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
